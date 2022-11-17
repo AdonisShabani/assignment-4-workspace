cd48e7d3c99a264808f33740325629dd6d535108
Adonis Shabani <adonis.shabani18@gmail.com>
2022-11-17 14:55:44 +0100

    edited s3 bucket

--- main.tf
+++ main.tf
@@ -6,11 +6,6 @@ resource "aws_s3_bucket" "b" {
   }
 }
 
-resource "aws_s3_bucket_acl" "b_acl" {
-  bucket = aws_s3_bucket.b.id
-  acl    = "private"
-}
-
 locals {
   s3_origin_id = "myS3Origin"
 }
