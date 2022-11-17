c4aeffc0f45ef3cc354ff8e78975348cb1a6a675
Adonis Shabani <adonis.shabani18@gmail.com>
2022-11-17 16:38:33 +0100

    added and object to s3

--- main.tf
+++ main.tf
@@ -5,10 +5,15 @@ resource "aws_s3_bucket" "b" {
     Name = "adonis-bucket-assignment"
   }
 }
-
-resource "aws_s3_bucket_acl" "b_acl" {
+resource "aws_s3_object" "object" {
   bucket = aws_s3_bucket.b.id
-  acl    = "private"
+  key    = "porfile"
+  source = "/Users/adonisshabani/Desktop/index.html"
+
+  # The filemd5() function is available in Terraform 0.11.12 and later
+  # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
+  # etag = "${md5(file("path/to/file"))}"
+  etag = filemd5("/Users/adonisshabani/Desktop/index.html")
 }
 
 locals {
