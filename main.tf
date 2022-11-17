5b1432213b1cef2841ef11139c236155721a6eda
Adonis Shabani <adonis.shabani18@gmail.com>
2022-11-17 16:50:05 +0100

    added key and origin

--- main.tf
+++ main.tf
@@ -6,14 +6,14 @@ resource "aws_s3_bucket" "b" {
   }
 }
 resource "aws_s3_object" "object" {
-  bucket = aws_s3_bucket.b.id
-  key    = "porfile"
-  source = "/Users/adonisshabani/Desktop/index.html"
+  bucket = aws_s3_bucket.b.bucket
+  key    = "Adonis99"
+  source = "./index.html"
 
   # The filemd5() function is available in Terraform 0.11.12 and later
   # For Terraform 0.11.11 and earlier, use the md5() function and the file() function:
   # etag = "${md5(file("path/to/file"))}"
-  etag = filemd5("/Users/adonisshabani/Desktop/index.html")
+  etag = filemd5("./index.html")
 }
 
 resource "aws_s3_bucket_acl" "b_acl" {
